f5ebdf0f457bb5fa5716139b04ba66208454a5b8
Gaetan Sorin <contact@gaetansorin.com>
2018-05-03 10:46:27 +0200

    Ajout initialisation du tableau de cellules
    Ajout tests initTab

--- src/Modele/Plateau/Plateau.java
+++ src/Modele/Plateau/Plateau.java
@@ -1,5 +1,9 @@
 package Modele.Plateau;
 
+import Utils.Position;
+
+import java.util.Random;
+
 public class Plateau {
 	private int size;
 	private Cellule[][] tab;
@@ -7,5 +11,23 @@ public class Plateau {
 	public Plateau(int size) {
 		this.size = size;
 		this.tab = new Cellule[size][size];
+		initTab();
+	}
+
+	/**
+	 * initTab : initialise le tableau selon une configuration attendue.
+	 */
+	private void initTab() {
+		Random r = new Random();
+		for (int i = 0; i < this.size; i++) {
+			for (int j = 0; j < this.size; j++) {
+				if(i % 2 == 0 && j == this.size-1) {
+					tab[i][j] = new Cellule(new Position(i,j),true);
+				}
+				else {
+					tab[i][j] = new Cellule(new Position(i,j));
+				}
+			}
+		}
 	}
 }
--- tests/Modele/Plateau/PlateauTest.java
+++ tests/Modele/Plateau/PlateauTest.java
@@ -1,11 +1,24 @@
 package Modele.Plateau;
 
+import Utils.Position;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 public class PlateauTest {
 	Plateau p;
 	@Before
 	public void setUp() {
 		p = new Plateau(3);
+	}
+
+	@Test
+	public void initTab() {
+		String expected = "{3, [" +
+				"[[(0,0), false], [(0,1), false], [(0,2), true]]," +
+				"[[(1,0), false], [(1,1), false], [(1,2), false]]," +
+				"[[(2,0), false], [(2,1), false], [(2,2), true]]," +
+				"}";
+		Assert.assertEquals(expected, p.toString());
 	}
 }
