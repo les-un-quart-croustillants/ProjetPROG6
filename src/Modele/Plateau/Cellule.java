2971662fd05f47b424a6a2e4283b3ebd70ce9aa9
Gaetan Sorin <contact@gaetansorin.com>
2018-05-03 10:42:26 +0200

    Ajout base classe cellule

---
+++ src/Modele/Plateau/Cellule.java
@@ -0,0 +1,17 @@
+package Modele.Plateau;
+
+import Utils.Position;
+
+public class Cellule {
+	Position position;
+
+	public Cellule() {
+		this(new Position(0,0));
+	}
+
+	public Cellule(Position position) {
+		this.position = position;
+		this.b = b;
+	}
+
+}
