7a8a164bcb99c9ce7e6be24f54cb0b9a4bf80835
Gaetan Sorin <contact@gaetansorin.com>
2018-05-04 11:25:13 +0200

    Ajout squelette JoueurPhysique

---
+++ src/Joueurs/JoueurPhysique.java
@@ -0,0 +1,11 @@
+package Joueurs;
+
+import Modele.Plateau.Plateau;
+import Utils.Position;
+
+public class JoueurPhysique extends Joueur {
+	@Override
+	public int jouerCoup(Plateau plateau,Pingouin pingouin, Position goal) {
+		return -1;
+	}
+}
