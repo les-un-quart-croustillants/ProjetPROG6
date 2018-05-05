19098a1a1b6114b81d3a126b1a29b764c2aa5e91
Louka Soret <louka.soret@gmail.com>
2018-05-05 17:44:30 +0200

    Ajout des tests JUnit pour jouerCoup() de JoueurPhysique

---
+++ src/Joueurs/JoueurPhysiqueTest.java
@@ -0,0 +1,81 @@
+package Joueurs;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import Modele.Plateau.Plateau;
+import Utils.Position;
+
+class JoueurPhysiqueTest {
+	JoueurPhysique j;
+	
+	@Before
+	public void setUp() {
+		ArrayList<Pingouin> s = new ArrayList<Pingouin>();
+		s.add(new Pingouin(0,new Position(1,1)));
+		s.add(new Pingouin(0,new Position(4,2)));
+		j = new JoueurPhysique(0,s);
+	}
+	
+	@Test
+	public void jouerCoup() {
+		Plateau plateau = new Plateau(10);;
+		Pingouin pingouin;
+		Position goal;
+		int expected;
+		
+		setUp();
+		
+		pingouin = j.squad().get(0);
+		goal = new Position(1,2);
+		expected = plateau.getCellule(goal).getFish();
+		try {
+			Assert.assertEquals("jouerCoup test #1/5 failed", expected, j.jouerCoup(plateau, pingouin, goal));
+		} catch (Exception e) {
+			fail("jouerCoup test #1/5 failed");
+		}
+		
+		pingouin = j.squad().get(0);
+		goal = new Position(1,-2);
+		expected = -1;
+		try {
+			Assert.assertEquals("jouerCoup test #2/5 failed", expected, j.jouerCoup(plateau, pingouin, goal));
+		} catch (Exception e) {
+			fail("jouerCoup test #2/5 failed");
+		}
+
+		pingouin = j.squad().get(0);
+		goal = new Position(1,-2);
+		expected = -1;
+		try {
+			Assert.assertEquals("jouerCoup test #3/5 failed", expected, j.jouerCoup(plateau, pingouin, goal));
+		} catch (Exception e) {
+			fail("jouerCoup test #3/5 failed");
+		}
+		
+		pingouin = new Pingouin(1,new Position(4,6));
+		goal = new Position(4,7);
+		try {
+			j.jouerCoup(plateau, pingouin, goal);
+			fail("jouerCoup test #4/5 failed");
+		} catch (Exception e) {
+			Assert.assertEquals(e.getMessage(), "Le pingouin en (4,6) n'appartient pas au joueur 0 mais au joueur 1.");
+		}
+		
+		pingouin = new Pingouin(0,new Position(4,6));
+		goal = new Position(4,7);
+		try {
+			j.jouerCoup(plateau, pingouin, goal);
+			fail("jouerCoup test #5/5 failed");
+		} catch (Exception e) {
+			Assert.assertEquals(e.getMessage(), "Le pingouin en (4,6) n'est pas reconnus par le joueur 0 (erreur interne).");
+		}
+		
+	}
+
+}
