79c6f3cae485a4704031e7a3a5262b9f7a42c287
Louka Soret <louka.soret@gmail.com>
2018-05-07 18:21:54 +0200

    Suppression du constructeur vide, modification de jouerCoup selon la nouvelle spec

--- src/Joueurs/JoueurPhysique.java
+++ src/Joueurs/JoueurPhysique.java
@@ -6,31 +6,28 @@ import Modele.Plateau.Plateau;
 import Utils.Position;
 
 public class JoueurPhysique extends Joueur {
-	public JoueurPhysique(){
-		super();
-	}
 	
 	public JoueurPhysique(int id){
 		super(id);
 	}
-
-	public JoueurPhysique(int id, ArrayList<Pingouin> s){
-		super(id,s);
-	}
 	
 	@Override
-	public int jouerCoup(Plateau plateau,Pingouin pingouin, Position goal) throws Exception {
+	public int jouerCoup(Plateau plateau,Position start, Position goal) throws Exception {
 		int res;
 		
-		if(pingouin.employeur() == this.id()) { //test si le pingouin appartient bien a ce joueur
-			if(this.squad().contains(pingouin)) { //test si le joueur reconnais bien le pingouin
-				res = plateau.jouer(this.squad().get(this.squad().indexOf(pingouin)), goal);
+		if(plateau.foo(start))) { //test si le pingouin existe
+			if(plateau.foo2(start).employeur() == this.id()) { //test si le pingouin appartient bien a ce joueur
+				res = plateau.jouer(start,goal);
+				if(res > 0) {
+					this.addScoreFish(res);	
+				}
+				this.addScoreDestroyed(1);
 				return res;
 			} else {
-				throw new Exception("Le pingouin en "+pingouin.position()+" n'est pas reconnus par le joueur "+this.id()+" (erreur interne).");
+				throw new Exception("Le pingouin en "+start+" n'appartient pas au joueur "+this.id()+".");
 			}
 		} else {
-			throw new Exception("Le pingouin en "+pingouin.position()+" n'appartient pas au joueur "+this.id()+" mais au joueur "+pingouin.employeur()+".");
+			throw new Exception("La case en "+start+" ne contient pas de pingouin.");
 		}
 	}
 }
