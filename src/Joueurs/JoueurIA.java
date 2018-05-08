4424900bb3ab5c999f14d4a2cef57df9e8b6cb35
Louka Soret <louka.soret@gmail.com>
2018-05-08 11:42:22 +0200

    Ajout d'un champ pingouinPos

--- src/Joueurs/JoueurIA.java
+++ src/Joueurs/JoueurIA.java
@@ -1,16 +1,29 @@
 package Joueurs;
 
+import java.util.ArrayList;
+
 import Modele.Plateau.Plateau;
 import Utils.Position;
 
 public class JoueurIA extends Joueur {
+	ArrayList<Position> pingouinsPos;
 
 	public JoueurIA(int id){
 		super(id);
+		this.pingouinsPos = new ArrayList<Position>();
 	}
 	
 	public JoueurIA(int id,int p){
 		super(id,p);
+		this.pingouinsPos = new ArrayList<Position>();
+	}
+	
+	public ArrayList<Position> pingouinsPos(){
+		return this.pingouinsPos;
+	}
+	
+	public void setPingouinsPos(ArrayList<Position> pp) {
+		this.pingouinsPos = pp;
 	}
 	
 	@Override
