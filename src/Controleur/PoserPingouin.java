f6c3671eb2a82616dc2a6e4f57eecfb357030074
Thomas Vandendorpe <vandendt@f203-23.e-im2ag.ujf-grenoble.fr>
2018-05-15 15:43:31 +0200

    printf de partie graphique enlevé

--- src/Controleur/PoserPingouin.java
+++ src/Controleur/PoserPingouin.java
@@ -30,14 +30,11 @@ public class PoserPingouin implements EventHandler<MouseEvent> {
 				if (c != null) {
 					int i_joueur_courant = GamePane.moteur().indexJoueurCourant();
 					if (GamePane.moteur().poserPingouin(c.posPlateau)) {
-						System.out.println("Le joueur " + GamePane.moteur().indexJoueurCourant() + "pose un pinguin en "
-								+ c.posPlateau);
 						GamePane.getPlateauCadre().gameObjects.add(
 								new PingouinGraphique(GamePane.moteur().plateau().getCellule(c.posPlateau).pingouin(),
 										pc.plateauGraphique, Donnees.COULEURS_JOUEURS[i_joueur_courant]));
 					} else {
-						System.out.println("Le joueur " + GamePane.moteur().indexJoueurCourant()
-								+ " ne peut pas poser un pinguin en " + c.posPlateau + " car la case est occupée");
+						
 					}
 				}
 			} else if (GamePane.moteur().currentState() == State.SELECTIONNER_PINGOUIN) {
@@ -55,19 +52,15 @@ public class PoserPingouin implements EventHandler<MouseEvent> {
 				Case c = pc.plateauGraphique.XYtoCase(new Point((int) event.getX(), (int) event.getY()));
 				Position lastSelection = GamePane.moteur().pingouinSelection().position();
 				Pingouin ping = GamePane.moteur().pingouinSelection();
-				System.out.println(ping);
 				LinkedList<Position> lastaccessibles = pc.plateau.accessible(lastSelection);
 				if (c != null && GamePane.moteur().selectionnerDestination(c.posPlateau)) {
-					System.out.println("truc");
 					GamePane.getPlateauCadre().plateauGraphique.cases[lastSelection.i()][lastSelection.j()].detruire();
 					for (Position pos : lastaccessibles) {
 						if(pc.plateauGraphique.cases[pos.i()][pos.j()]!=null)
 							pc.plateauGraphique.cases[pos.i()][pos.j()].deselect();
 					}
-					System.out.println(ping);
 				}
 				else {
-					System.out.println(GamePane.moteur().currentState());
 					for (Position pos : lastaccessibles) {
 						if(pc.plateauGraphique.cases[pos.i()][pos.j()]!=null)
 							pc.plateauGraphique.cases[pos.i()][pos.j()].deselect();
