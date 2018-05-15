5195fc9881b46ac8e47b39f3afa33b610d42d32c
Gaetan Sorin <contact@gaetansorin.com>
2018-05-15 09:54:39 +0200

    Simplification de Move à deux positions

--- src/Modele/Plateau/Move.java
+++ src/Modele/Plateau/Move.java
@@ -3,52 +3,51 @@ package Modele.Plateau;
 import Utils.Position;
 
 public class Move {
-	private Pingouin penguin;
-	private Position target;
-	private Position previous;
+	private Position to;
+	private Position from;
 	private int fishAte;
 
-	public Move(Pingouin penguin, Position target, Position previous, int fishAte ) {
-		this.penguin = penguin;
-		this.target = target;
-		this.previous = previous;
+	public Move(Position to, Position from, int fishAte ) {
+		this.to = to;
+		this.from = from;
 		this.fishAte = fishAte;
 	}
 
-	public Pingouin getPenguin() {
-		return penguin;
-	}
 
-	public Position getPrevious() {
-		return previous;
+	public Position getFrom() {
+		return from;
 	}
 
 	public int getFishAte() {
 		return fishAte;
 	}
 
-	public Position getTarget() {
-		return target;
+	public Position getTo() {
+		return to;
 	}
 
-	public void setPenguin(Pingouin penguin) {
-		this.penguin = penguin;
-	}
 
-	public void setPrevious(Position previous) {
-		this.previous = previous;
+	public void setFrom(Position from) {
+		this.from = from;
 	}
 
 	public void setFishAte(int fishAte) {
 		this.fishAte = fishAte;
 	}
 
-	public void setTarget(Position target) {
-		this.target = target;
+	public void setTo(Position to) {
+		this.to = to;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return this.from.equals(((Move) obj).getFrom())
+				&& this.to.equals(((Move) obj).getTo())
+				&& this.fishAte == ((Move) obj).getFishAte();
 	}
 
 	@Override
 	public String toString() {
-		return this.penguin + "," + target + "," + previous + "," + fishAte;
+		return to + "," + from + "," + fishAte;
 	}
 }
