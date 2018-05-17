de597338e5e732dd460ec31c22aa11721a12e3d2
Thomas Vandendorpe <vandendt@f203-23.e-im2ag.ujf-grenoble.fr>
2018-05-17 16:42:06 +0200

    début affichage score

--- src/Vue/GameObject/ScoresGraphique.java
+++ src/Vue/GameObject/ScoresGraphique.java
@@ -1,11 +1,12 @@
 package Vue.GameObject;
 
+import Vue.Donnees;
 import Vue.Pane.GamePane;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 
 public class ScoresGraphique extends GameObject{
-	
-	
+		
 	public ScoresGraphique(){
 	}
 	
@@ -16,8 +17,15 @@ public class ScoresGraphique extends GameObject{
 	
 	@Override
 	public void draw(GraphicsContext gc){
+		String nom,score;
+		int w = (int) (GamePane.getPlateauCadre().getWidth() * 0.15);
 		for(int i=0;i<GamePane.moteur().njoueurs();i++){
-			gc.fillText("TRUC", (GamePane.getPlateauCadre().getWidth()/(GamePane.moteur().njoueurs()+1))*(i+1), GamePane.getPlateauCadre().getHeight()*0.9);
+			nom = GamePane.moteur().joueur(i).nom();
+			score = "x"+Integer.toString(GamePane.moteur().joueur(i).scoreFish());
+			gc.setFill(Donnees.COULEURS_JOUEURS[GamePane.moteur().joueur(i).id()]);
+			gc.fillText(nom, (GamePane.getPlateauCadre().getWidth()/(GamePane.moteur().njoueurs()+1))*(i+1)-nom.length()*gc.getFont().getSize()*0.25, GamePane.getPlateauCadre().getHeight()*0.9,w);
+			gc.setFill(Color.WHITE);
+			gc.fillText(score, (GamePane.getPlateauCadre().getWidth()/(GamePane.moteur().njoueurs()+1))*(i+1)-score.length()*gc.getFont().getSize()*0.25, GamePane.getPlateauCadre().getHeight()*0.95,w);
 		}
 		
 		
