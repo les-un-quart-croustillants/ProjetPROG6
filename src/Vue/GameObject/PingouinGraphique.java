05df58feb1c6422d153a2a7c8123e5748be53644
ThomasVandendorpe <tvand@DESKTOP-FQAIH9S>
2018-05-21 22:03:13 +0200

    Ajout popup parametre + pingouin plus gros

--- src/Vue/GameObject/PingouinGraphique.java
+++ src/Vue/GameObject/PingouinGraphique.java
@@ -2,9 +2,11 @@ package Vue.GameObject;
 
 import com.sun.javafx.geom.Vec2d;
 
+import Modele.Moteur.Moteur.State;
 import Modele.Plateau.Pingouin;
 import Utils.Position;
 import Vue.Donnees;
+import Vue.Pane.GamePane;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
@@ -13,7 +15,7 @@ public class PingouinGraphique extends GameObject {
 	PlateauGraphique pg;
 	Color couleur;
 	Vec2d offset = new Vec2d(-Donnees.IMG_PINGOUIN_FACE.getWidth()/2,-Donnees.IMG_PINGOUIN_FACE.getHeight());
-	float scale = 0.7f;
+	float scale = 1f;
 	int width,height;
 	Position pingouinPosition;
 	
@@ -25,6 +27,7 @@ public class PingouinGraphique extends GameObject {
 		pingouinPosition = new Position(pingouin.position().i(), pingouin.position().j());
 	}
 	
+	private float f = 0.5f;
 	@Override
 	public void update() {
 		position.x = pg.cases[pingouinPosition.i()][pingouinPosition.j()].position().x+pg.tailleCase/2;
@@ -33,11 +36,17 @@ public class PingouinGraphique extends GameObject {
 		offset.y = -Donnees.IMG_PINGOUIN_FACE.getHeight() * scale;
 		width = (int) (Donnees.IMG_PINGOUIN_FACE.getWidth()*scale);
 		height = (int) (Donnees.IMG_PINGOUIN_FACE.getHeight()*scale);
+		if(GamePane.moteur().currentState()==State.SELECTIONNER_PINGOUIN && !GamePane.moteur().joueur(pingouin.employeur()).estIA() && pingouin.employeur() == GamePane.moteur().joueurCourant().id())
+			f = (float) (10 + Math.cos(0.01*System.currentTimeMillis()) % 1)/10;
+		else
+			f = 1;
 	}
 	@Override
 	public void draw(GraphicsContext gc) {
 		gc.setFill(couleur);
-		gc.fillOval(position.x-pg.tailleCase*0.3, position.y-pg.tailleCase*0.15, pg.tailleCase*0.6, pg.tailleCase*0.2);
+		gc.setGlobalAlpha(0.5);
+		gc.fillOval(position.x-pg.tailleCase*0.3*f, position.y-pg.tailleCase*0.2*f, pg.tailleCase*0.6*f, pg.tailleCase*0.3*f);
+		gc.setGlobalAlpha(1);
 		gc.drawImage(Donnees.IMG_PINGOUIN_FACE, position.x+offset.x, position.y+offset.y, width, height);
 	}
 	
