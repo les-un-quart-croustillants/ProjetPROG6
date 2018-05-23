2f85376781b97a2cfb4a277da48f3cbd5ee05409
Gaetan Sorin <contact@gaetansorin.com>
2018-05-23 12:11:07 +0200

    Ajout pingouin dans Move
    Test de la nouvelle structure Move

--- src/Modele/Plateau/Move.java
+++ src/Modele/Plateau/Move.java
@@ -8,11 +8,21 @@ public class Move implements Serializable {
 	private Position to;
 	private Position from;
 	private int fishAte;
+	private Pingouin pingouin;
 
-	public Move(Position to, Position from, int fishAte ) {
+	public Move(Position to, Pingouin pingouin) {
+		this(to, Plateau.source, 1, pingouin);
+	}
+
+	public Move(Position to, Position from, int fishAte) {
+		this(to, from, fishAte, null);
+	}
+
+	public Move(Position to, Position from, int fishAte, Pingouin pingouin) {
 		this.to = to;
 		this.from = from;
 		this.fishAte = fishAte;
+		this.pingouin = pingouin;
 	}
 
 
@@ -28,15 +38,38 @@ public class Move implements Serializable {
 		return to;
 	}
 
+	public boolean aPingouin() {
+		return pingouin != null;
+	}
+
+	public Pingouin getPingouin() {
+		return pingouin;
+	}
+
 	@Override
-	public boolean equals(Object obj) {
-		return this.from.equals(((Move) obj).getFrom())
-				&& this.to.equals(((Move) obj).getTo())
-				&& this.fishAte == ((Move) obj).getFishAte();
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Move)) return false;
+
+		Move move = (Move) o;
+
+		if (getFishAte() != move.getFishAte()) return false;
+		if (!getTo().equals(move.getTo())) return false;
+		if (!getFrom().equals(move.getFrom())) return false;
+		return getPingouin() != null ? getPingouin().equals(move.getPingouin()) : move.getPingouin() == null;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = getTo().hashCode();
+		result = 31 * result + getFrom().hashCode();
+		result = 31 * result + getFishAte();
+		result = 31 * result + (getPingouin() != null ? getPingouin().hashCode() : 0);
+		return result;
 	}
 
 	@Override
 	public String toString() {
-		return to + "," + from + "," + fishAte;
+		return to + "," + from + "," + fishAte + "," + pingouin;
 	}
 }
--- tests/Modele/Plateau/MoveTest.java
+++ tests/Modele/Plateau/MoveTest.java
@@ -59,11 +59,12 @@ public class MoveTest {
 		Position to1 = new Position(0,0),
 				to2 = new Position(0,0),
 				from = new Position(r.nextInt(), r.nextInt());
+		Pingouin pingouin = new Pingouin(r.nextInt());
 		int nb_fish = r.nextInt();
 		Move m1, m2;
 		for (int i = 0; i < 100; i++) {
-			m1 = new Move(to1, from, nb_fish);
-			m2 = new Move(to2, from, nb_fish);
+			m1 = new Move(to1, from, nb_fish, pingouin);
+			m2 = new Move(to2, from, nb_fish, pingouin);
 			Assert.assertEquals(m1, m1);
 			Assert.assertEquals(m2,m2);
 			Assert.assertEquals(to1.equals(to2),m1.equals(m2));
@@ -79,10 +80,11 @@ public class MoveTest {
 				from2 = new Position(0,0),
 				to = new Position(r.nextInt(), r.nextInt());
 		Move m1, m2;
+		Pingouin pingouin = new Pingouin(r.nextInt());
 		int nb_fish = r.nextInt();
 		for (int i = 0; i < 100; i++) {
-			m1 = new Move(to, from1, nb_fish);
-			m2 = new Move(to, from2, nb_fish);
+			m1 = new Move(to, from1, nb_fish, pingouin);
+			m2 = new Move(to, from2, nb_fish, pingouin);
 			Assert.assertEquals(m1, m1);
 			Assert.assertEquals(m2,m2);
 			Assert.assertEquals(from1.equals(from2),m1.equals(m2));
@@ -97,16 +99,38 @@ public class MoveTest {
 		Position to = new Position(r.nextInt(), r.nextInt()),
 				from = new Position(r.nextInt(), r.nextInt());
 		Move m1, m2;
+		Pingouin pingouin = new Pingouin(r.nextInt());
 		int nb_1 = 0,
 			nb_2 = 0;
 		for (int i = 0; i < 100; i++) {
-			m1 = new Move(to, from, nb_1);
-			m2 = new Move(to, from, nb_2);
+			m1 = new Move(to, from, nb_1, pingouin);
+			m2 = new Move(to, from, nb_2, pingouin);
 			Assert.assertEquals((nb_1 == nb_2), m1.equals(m2));
 			nb_2 = r.nextInt();
 			nb_1 = r.nextInt();
 		}
 
+	}
+	private void move_equals_employeur() {
+		Random r = new Random();
+		Position to = new Position(r.nextInt(), r.nextInt()),
+				from = new Position(r.nextInt(), r.nextInt());
+		Move m1, m2;
+		Pingouin pingouin_1 = new Pingouin(r.nextInt()),
+				pingouin_2 = new Pingouin(r.nextInt());
+		;
+		int nb = r.nextInt(3) + 1;
+		for (int i = 0; i < 100; i++) {
+			m1 = new Move(to, from, nb, pingouin_1);
+			m2 = new Move(to, from, nb, pingouin_2);
+			Assert.assertEquals((pingouin_1.equals(pingouin_2)), m1.equals(m2));
+			pingouin_1 = new Pingouin(r.nextInt());
+			pingouin_2 = new Pingouin(r.nextInt());
+		}
+
+		m1 = new Move(to, from, nb, pingouin_1);
+		m2 = new Move(to, from, nb, null);
+		Assert.assertNotEquals(m1,m2);
 	}
 
 	@Test
@@ -114,6 +138,7 @@ public class MoveTest {
 		move_equals_to();
 		move_equals_from();
 		move_equals_nb_fish();
+		move_equals_employeur();
 	}
 
 }
