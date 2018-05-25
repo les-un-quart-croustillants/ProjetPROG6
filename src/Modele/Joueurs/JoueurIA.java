5e796552860a9260549f8ea3d70da17ebf6dee62
Louka Soret <louka.soret@gmail.com>
2018-05-25 11:23:58 +0200

    Ia ne freeze plus l'ihm

--- src/Modele/Joueurs/JoueurIA.java
+++ src/Modele/Joueurs/JoueurIA.java
@@ -2,6 +2,8 @@ package Modele.Joueurs;
 
 import java.util.ArrayList;
 import java.lang.Thread;
+
+import Modele.Joueurs.Joueur.Difficulte;
 import Modele.Plateau.Plateau;
 import Utils.Couple;
 import Utils.Position;
@@ -9,10 +11,15 @@ import Utils.Position;
 
 public class JoueurIA extends Joueur {
 	
+	private boolean threadLancee;
+	private IAshared shared;
+	
 	private static final long serialVersionUID = -8134226007569319548L;
 
 	public JoueurIA(int id,int nbP,String n,Difficulte d){
 		super(id,nbP,n,d);
+		this.shared = new IAshared();
+		this.threadLancee = false;
 	}
 
 	@Override
@@ -30,18 +37,18 @@ public class JoueurIA extends Joueur {
 	}
 	
 	
-	
 	@Override
 	public Couple<Position,Position> prochainCoup(Plateau plateau,ArrayList<ArrayList<Integer>> scores) {
-		switch(this.difficulte) {
-			case FACILE:
-				return UtilsIA.jouerCoupFacile(plateau,super.id());
-			case MOYEN:
-				return UtilsIA.jouerCoupFacile(plateau,super.id());
-			case DIFFICILE:
-				return UtilsIA.jouerCoupDifficile(plateau,super.id(),scores);
-			default :
-				return UtilsIA.jouerCoupFacile(plateau,super.id());
+		if(this.threadLancee) {
+			Couple<Position,Position> tmp = this.shared.getCoupCalcule();
+			if(tmp != null) {
+				this.threadLancee = false;
+			}
+			return tmp;
+		} else {
+			new IAthread(super.difficulte,plateau, super.id(),scores,this.shared);
+			this.threadLancee = true;
+			return null;
 		}
 	}
 	/*
@@ -64,7 +71,17 @@ public class JoueurIA extends Joueur {
 	
 	@Override
 	public Position prochainePosePingouin(Plateau plateau) {
-		return UtilsIA.bestplace(plateau, super.id());
+		if(this.threadLancee) {
+			Position tmp = this.shared.getPoseCalcule();
+			if(tmp != null) {
+				this.threadLancee = false;
+			}
+			return tmp;
+		} else {
+			new IAthread(super.difficulte,plateau, super.id(),this.shared);
+			this.threadLancee = true;
+			return null;
+		}
 	}
 	
 	@Override
